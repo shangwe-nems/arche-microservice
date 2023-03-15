37bec67f77433892dc54a98d6762dd59cca2ca9f
shangwe-nems <shangwe.dev@gmail.com>
2023-03-15 14:50:01 -0700

    Initial commit

---
+++ apps/billing/src/billing.module.ts
@@ -0,0 +1,22 @@
+import { RqmModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+    RqmModule,
+  ],
+  controllers: [BillingController],
+  providers: [BillingService],
+})
+export class BillingModule {}
